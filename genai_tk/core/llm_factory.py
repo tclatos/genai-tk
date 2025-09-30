89b579151e02204a8dd3dfcc662ebb9cc3f65166
tcaminel-pro <thierry.caminel@eviden.com>
2025-09-30 12:11:06 +0200

    Remove unused devtools import
    - Removed unused devtools import that was causing import errors
    - Import was commented as noqa F401 indicating it was already known to be unused

--- genai_tk/core/llm_factory.py
+++ genai_tk/core/llm_factory.py
@@ -43,7 +43,7 @@ from functools import cached_property, lru_cache
 from typing import Annotated, Any, cast
 
 import yaml
-from devtools import debug  # noqa: F401
+# from devtools import debug  # noqa: F401 - removed unused import
 from langchain.chat_models.base import init_chat_model
 from langchain_core.language_models.base import BaseLanguageModel
 from langchain_core.language_models.chat_models import BaseChatModel
